6176bcf1185eee9fe687a68cf0c458cbf45678a5
Ryan Carpenter <rccarpenter@outlook.com>
2021-02-02 10:56:39 -0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+//NOTE - TypeScript Decoratator - includes metadata for component
 @Component({
+  //NOTE - used in selector (html element where to load)
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todo-list';
+  title: string = 'angular-todo-list';
+
+  constructor() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    //NOTE - Components go here (all components are TS files instead of JS)
+    AppComponent,
+    TodoListComponent
   ],
   imports: [
+    //NOTE - Modules (like HTTP Module) go here
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    //NOTE - Services you want to include
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
